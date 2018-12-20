9a05fe8bd08111dff7e3a4bcf5b8e2b0765424c0
saravyas <saravyas007@gmail.com>
2018-12-20 07:25:08 +0530

    express server setup

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-const check = () => {
-  console.log("getting started");
-};
-check();
+import express from "express";
+import bodyParser from "body-parser";
+import logger from "morgan";
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(8080);
